c599dc6f86cef072ce3b6ff19db11763989b026b
Lucas Marçal <lucasmarcal@gmail.com>
2023-05-04 15:19:25 +0300

    Navbar componentization.

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,36 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+import marshallIcon from "../../public/img/marshall-icn-preto.png";
+import Image from "next/image";
+import { ShoppingCartIcon } from "@heroicons/react/24/outline";
+
+const Navbar: React.FC = () => {
+  const { data: sessionData } = useSession();
+
+  return (
+    <nav
+      className="sticky top-0 z-10 flex justify-center space-x-0 bg-red-600 px-5 align-middle"
+      onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+    >
+      <div className="flex w-full max-w-2xl justify-between space-x-3 py-2">
+        <Image alt="Marshall icon" src={marshallIcon} className="h-auto w-5" />
+        <ul className="flex justify-center space-x-4 align-middle">
+          <li className="self-center">
+            <button
+              className="rounded-lg bg-red-700 px-7 py-3 font-semibold text-white no-underline transition hover:bg-red-800"
+              onClick={sessionData ? () => void signOut() : () => void signIn()}
+            >
+              {sessionData ? "Sair" : "Entrar"}
+            </button>
+          </li>
+          <li className="self-center">
+            <button className="group rounded-full bg-neutral-900 p-3 no-underline">
+              <ShoppingCartIcon className="h-6 w-6 stroke-red-600 transition group-hover:stroke-neutral-50" />
+            </button>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,7 @@ import { RouterOutputs, api } from "~/utils/api";
 import Image from "next/image";
 import { ShoppingCartIcon } from "@heroicons/react/24/outline";
 import { useState } from "react";
+import Navbar from "~/components/Navbar";
 
 const Home: NextPage = () => {
   const { data: sessionData } = useSession();
@@ -26,35 +27,7 @@ const Home: NextPage = () => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <nav
-        className="sticky top-0 z-10 flex justify-center space-x-0 bg-red-600 px-5 align-middle"
-        onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
-      >
-        <div className="flex w-full max-w-2xl justify-between space-x-3 py-2">
-          <Image
-            alt="Marshall icon"
-            src={marshallIcon}
-            className="h-auto w-5"
-          />
-          <ul className="flex justify-center space-x-4 align-middle">
-            <li className="self-center">
-              <button
-                className="rounded-lg bg-red-700 px-7 py-3 font-semibold text-white no-underline transition hover:bg-red-800"
-                onClick={
-                  sessionData ? () => void signOut() : () => void signIn()
-                }
-              >
-                {sessionData ? "Sair" : "Entrar"}
-              </button>
-            </li>
-            <li className="self-center">
-              <button className="group rounded-full bg-neutral-900 p-3 no-underline">
-                <ShoppingCartIcon className="h-6 w-6 stroke-red-600 transition group-hover:stroke-neutral-50" />
-              </button>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <Navbar />
       <main className="relative flex min-h-screen flex-col items-center bg-neutral-900 pb-14 pt-5">
         <div className="max-w-sm px-5">
           <Image alt="Marshall logo" src={marshallogo} className="max-w-5" />
